fa48da001e9ff1f7e61b10cbd3de58c37da99fe8
Kelly <84016657+telkelly@users.noreply.github.com>
2023-04-03 17:54:12 +0300

    started exercises

---
+++ exercisesXP/script.js
@@ -0,0 +1,21 @@
+// exercise-1 part-1
+
+const infoAboutMe = () => {
+    return console.log("My name is Kelly, I'm 23 years old, my fav color is orange");
+
+}
+infoAboutMe() 
+
+// part-2
+
+const infoAboutPerson = (name, age, favColor) => {
+  return console.log(
+    `My name is ${name}, I'm ${age} years old, my fav color is ${favColor}`
+  );
+};
+
+infoAboutPerson("David", 45, "blue");
+infoAboutPerson("Josh", 12, "yellow");
+
+// exercise-2
+
